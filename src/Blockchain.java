65e28c82f204f8e8220261094ded209360256431
Alex Lukash <2hala2real2madrid2@gmail.com>
2021-12-14 01:31:39 +0300

    Easy Blockchain

---
+++ src/Blockchain.java
@@ -0,0 +1,12 @@
+public class Blockchain {
+    public static void main(String[] args){
+        Block firstBlock = new Block("First block", "0");
+        System.out.println("Hash for block 1: " + firstBlock.hash);
+
+        Block secondBlock = new Block("Second block", firstBlock.hash);
+        System.out.println("Hash for block 2: " + secondBlock.hash);
+
+        Block thirdBlock = new Block("Third block", secondBlock.hash);
+        System.out.println("Hash for block 3: " + thirdBlock.hash);
+    }
+}
